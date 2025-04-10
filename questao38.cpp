756a019d088fa18a0287fbdc990002536a96352f
artPerozin <perozin.arthur@gmail.com>
2025-04-09 22:44:38 -0300

    adiciona questao 38

---
+++ questao38.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1 = 0, n2 = 1, next;
+
+    cout << "Os primeiros 30 numeros de Fibonacci sao:" << endl;
+    
+    for (int i = 0; i < 30; i++) {
+        cout << n1 << " ";
+        next = n1 + n2;
+
+        n1 = n2;
+        n2 = next;
+    }
+
+    cout << endl;
+    return 0;
+}
